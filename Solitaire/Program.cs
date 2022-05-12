e7c49543c9d99d9dd4ac0f0deb3fb6b3bd18f523
Jens Jensen <jej@eucnord.dk>
2022-05-12 09:14:39 +0200

    Add project files.

---
+++ Solitaire/Program.cs
@@ -0,0 +1,15 @@
+﻿using Solitaire;
+
+Card card1 = new();
+Card card2 = new();
+card1.Suit = 1;
+card1.Value = 2;
+card2.Suit = 4;
+card2.Value = 13;
+
+Console.WriteLine(card1.Suit);
+Console.WriteLine(card1.Value);
+Console.WriteLine(card2.Suit);
+Console.WriteLine(card2.Value);
+
+
